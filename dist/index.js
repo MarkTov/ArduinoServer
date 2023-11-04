471c4086b1d657d2ed2bdeffdac89afd396c3111
MarkTov <149862141+MarkTov@users.noreply.github.com>
2023-11-04 09:10:10 +0200

    portfix2

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const app_1 = require("./app/app");
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv = require("dotenv").config();
 console.log("Starting app");
-app_1.app.listen(Number(dotenv.parsed.PORT), () => {
+app_1.app.listen(Number(3001), () => {
     console.log(`Server runnig on port ${dotenv.parsed.PORT}`);
 });
 mongoose_1.default.set("strictQuery", false);
